ccc1fe6116141a1f379b518b357f116190161fda
randolph.law <randolphlawlaw@gmail.com>
2024-08-30 13:57:00 +0800

    20240830

---
+++ futu_api/src/test/java/futu_api/futu_api/FutuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package futu_api.futu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
